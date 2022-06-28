1536a306799a51684e03cabb3818dfeafa484d05
Yehonatan <yehonatan349@gmail.com>
2022-06-28 16:00:13 +0300

    ADD: Delete button

--- tabs.js
+++ tabs.js
@@ -5,6 +5,7 @@ form.addEventListener('submit', function (event) {
     let fname = document.getElementById('fullName').value;
     let job = document.getElementById('job').value;
     let email = document.getElementById('email').value;
+    let cardContainer = document.getElementById('cardContainer')
 
     if (fname.length < 2) {
         alert('Your name is less than 2 characters.');
@@ -12,10 +13,11 @@ form.addEventListener('submit', function (event) {
     if (!email.includes("@")) {
         alert('The email is not valid.');
     }
-    
+
     if (fname.length >= 2 && email.includes("@")) {
         const cardDiv = document.createElement("div");
         cardDiv.classList.add('card')
+        cardDiv.setAttribute("id", fname)//TODO: How to create dimanic number 
         const cardName = document.createElement("p");
         cardName.classList.add('fullName')
         cardName.innerHTML = fname;
@@ -26,9 +28,27 @@ form.addEventListener('submit', function (event) {
         cardEmail.classList.add('email')
         cardEmail.innerHTML = email;
 
+        const btn = createDelBtn();
+
         cardDiv.appendChild(cardName);
         cardDiv.appendChild(cardJob);
         cardDiv.appendChild(cardEmail);
-        document.body.append(cardDiv);
+        cardDiv.appendChild(btn);
+        cardContainer.appendChild(cardDiv);
     }
 })
+
+function createDelBtn() {
+    var button = document.createElement("button");
+    button.classList.add('deleteBtn')
+    button.innerHTML = "Delete";
+
+    var body = document.getElementsByTagName("body")[0];
+    body.appendChild(button);
+
+    button.addEventListener("click", function (event) {
+        const elem = document.getElementById(event.target.parentElement.id);
+        elem.remove();
+    })
+    return button;
+};
