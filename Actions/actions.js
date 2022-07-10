acfbfcf3a9d563b2b9c79bfae47d27d2110f6b87
Yehonatan <yehonatan349@gmail.com>
2022-07-10 14:04:19 +0300

    Formatting

--- Actions/actions.js
+++ Actions/actions.js
@@ -101,11 +101,10 @@ const usersArray = [
     },
 ]
 
-var cardContainer = document.getElementById('cardContainer');
-createCard(usersArray)
+createCards(usersArray)
 
-function createCard(usersArray) {
-    for (let i = 0; i < usersArray.length; i++) {
+function createCards(usersArray) {
+    for (i = 0; i < usersArray.length; i++) {
         const cardDiv = document.createElement("div");
         cardDiv.classList.add('card');
         cardDiv.setAttribute("id", usersArray[i].name);
@@ -132,18 +131,18 @@ function createCard(usersArray) {
         const myJSON = JSON.stringify(usersArray[i].address);
         address.innerHTML = `Address: ${myJSON}`;
 
-        cardDiv.appendChild(name);
-        cardDiv.appendChild(age);
-        cardDiv.appendChild(isAdmin);
-        cardDiv.appendChild(grades);
-        cardDiv.appendChild(address);
-        cardContainer.appendChild(cardDiv);
+        addCardToDom(cardDiv, name, age, isAdmin, grades, address)
     }
 }
 
-
-
-
+function addCardToDom(cardDiv, name, age, isAdmin, grades, address) {
+    cardDiv.appendChild(name);
+    cardDiv.appendChild(age);
+    cardDiv.appendChild(isAdmin);
+    cardDiv.appendChild(grades);
+    cardDiv.appendChild(address);
+    cardContainer.appendChild(cardDiv);
+}
 
 function getFilterValue() {
     return document.getElementById('filter-value').value;
@@ -161,184 +160,155 @@ function getSelectorValue() {
     return document.getElementById('fileds').value;
 }
 
-
 function averageGrades(grades) {
     let averageGrades = 0;
-    for (let j = 0; j < grades.length; j++) {
+    for (j = 0; j < grades.length; j++) {
         averageGrades = averageGrades + grades[j];
     }
     averageGrades = averageGrades / (grades.length + 1);
     return averageGrades;
 }
 
-
-var form = document.getElementById('find');
-form.addEventListener('click', function (event) {
+let formValue = document.getElementById('select-filter');
+formValue.addEventListener('submit', function (event) {
     event.preventDefault();
     let selector = getSelectorValue();
     let filterValue = getFilterValue();
+    if (event.submitter.id == 'find') {
+        switch (selector) {
 
-    switch (selector) {
-        case 'Age':
-            let filterValue = getFilterValue();
+            case 'Age':
+                filterValue = parseInt(filterValue);
+                if (isFilterValueNaN(filterValue)) {
+                    alert('Please insert a number');
+                } else {
+                    for (i = 0; i < usersArray.length; i++) {
+                        if (filterValue > usersArray[i].age) {
+                            document.getElementById(usersArray[i].name).style.display = "none";
+                        }
+                    }
+                }
+                break;
 
-            filterValue = parseInt(filterValue);
-            if (isFilterValueNaN(filterValue)) {
-                alert('Please insert a number');
-            } else {
-                for (let i = 0; i < usersArray.length; i++) {
-                    if (filterValue > usersArray[i].age) {
+            case 'Name':
+            filterValue = filterValue.toLowerCase();
+                for (i = 0; i < usersArray.length; i++) {
+                    if (filterValue != usersArray[i].name) {
                         document.getElementById(usersArray[i].name).style.display = "none";
                     }
                 }
-            }
-            break;
+                break;
 
-        case 'Name': //TODO: MAKE case sensetive
-            for (let i = 0; i < usersArray.length; i++) {
-                if (filterValue != usersArray[i].name) {
-                    document.getElementById(usersArray[i].name).style.display = "none";
-                }
-            }
-            break;
-
-        case 'Admin':
-            filterValue = filterValue.toLowerCase()
-            if (filterValue == "true") {
-                for (let i = 0; i < usersArray.length; i++) {
-                    if (usersArray[i].admin == false) {
-                        document.getElementById(usersArray[i].name).style.display = "none";
+            case 'Admin':
+                filterValue = filterValue.toLowerCase()
+                if (filterValue == "true") {
+                    for (i = 0; i < usersArray.length; i++) {
+                        if (usersArray[i].admin == false) {
+                            document.getElementById(usersArray[i].name).style.display = "none";
+                        }
                     }
                 }
-            }
-            else if (filterValue == "false") {
-                for (let i = 0; i < usersArray.length; i++) {
-                    if (usersArray[i].admin == true) {
-                        document.getElementById(usersArray[i].name).style.display = "none";
+                else if (filterValue == "false") {
+                    for (i = 0; i < usersArray.length; i++) {
+                        if (usersArray[i].admin == true) {
+                            document.getElementById(usersArray[i].name).style.display = "none";
+                        }
+                    }
+                } else {
+                    alert('Only True or False')
+                }
+                break;
+
+            case 'Grades':
+                filterValue = parseInt(filterValue);
+                if (isFilterValueNaN(filterValue)) {
+                    alert('Please insert a number');
+                } else {
+                    for (i = 0; i < usersArray.length; i++) {
+                        let avgGraed = averageGrades(usersArray[i].grades)
+                        if (avgGraed < filterValue) {
+                            document.getElementById(usersArray[i].name).style.display = "none";
+                        }
                     }
                 }
-            } else {
-                alert('Only True or False')
-            }
-            break;
+                break;
 
-        case 'Grades':
-            filterValue = parseInt(filterValue);
-            if (isFilterValueNaN(filterValue)) {
-                alert('Please insert a number');
-            } else {
-                for (let i = 0; i < usersArray.length; i++) {
-                    let avgGraed = averageGrades(usersArray[i].grades)
-                    if (avgGraed < filterValue) {
-                        document.getElementById(usersArray[i].name).style.display = "none";
+            case 'Address':
+                var myArray = filterValue.split(".");
+                myArray[0] = myArray[0].toLowerCase()
+                if (myArray[0] == 'city') {
+                    for (i = 0; i < usersArray.length; i++) {
+                        if (myArray[1] != usersArray[i].address.city) {
+                            document.getElementById(usersArray[i].name).style.display = "none";
+                        }
                     }
                 }
-            }
-            break;
-
-        case 'Address':
-            var myArray = filterValue.split(".");
-            myArray[0] = myArray[0].toLowerCase()
-
-
-            if (myArray[0] == 'city') {
-                for (let i = 0; i < usersArray.length; i++) {
-                    if (myArray[1] != usersArray[i].address.city) {
-                        document.getElementById(usersArray[i].name).style.display = "none";
-                    }
-                }
-            }
-
-            else if (myArray[0] == 'housenumber') {
-                myArray[1] = parseInt(myArray[1]);
-
-                if (typeof myArray[1] == 'number') {
-                    if (!isNaN(myArray[1])) {
-                        for (let i = 0; i < usersArray.length; i++) {
-                            if (myArray[1] != usersArray[i].address.houseNumber) {
-                                document.getElementById(usersArray[i].name).style.display = "none";
+                else if (myArray[0] == 'housenumber') {
+                    myArray[1] = parseInt(myArray[1]);
+                    if (typeof myArray[1] == 'number') {
+                        if (!isNaN(myArray[1])) {
+                            for (i = 0; i < usersArray.length; i++) {
+                                if (myArray[1] != usersArray[i].address.houseNumber) {
+                                    document.getElementById(usersArray[i].name).style.display = "none";
+                                }
                             }
+                        } else {
+                            alert('House Number Takes only numbers');
                         }
                     } else {
                         alert('House Number Takes only numbers');
-
                     }
+
                 } else {
-                    alert('House Number Takes only numbers');
-
+                    alert('Only City or House Number');
                 }
-
-            } else {
-                alert('Only City or House Number');
-
-            }
-            break;
-    }
-}
-)
-
-var form = document.getElementById('all-grades-greater-than');
-form.addEventListener('click', function (event) {
-    event.preventDefault();
-    let filterValue = getFilterValue();
-    let FilterValueNaN = isFilterValueNaN(filterValue);
-
-    if (FilterValueNaN) {
-        alert('Numbers Only');
-    } else {
-        for (let i = 0; i < usersArray.length; i++) {
-            if ((Math.min(...usersArray[i].grades)) < filterValue) {
-                document.getElementById(usersArray[i].name).style.display = "none";
-            }
+                break;
         }
-    }
-}
-)
-
-var form = document.getElementById('some-grades-greater-than');
-form.addEventListener('click', function (event) {
-    event.preventDefault();
-    let filterValue = getFilterValue();
-    let FilterValueNaN = isFilterValueNaN(filterValue);
-
-    if (FilterValueNaN) {
-        alert('Numbers Only');
-    } else {
-        for (let i = 0; i < usersArray.length; i++) {
-            for (let j = 0; j < usersArray[i].grades[j]; j++) {
-                if (usersArray[i].grades[j] > filterValue) {
-                    break;
-                } else if (j + 1 == usersArray[i].grades.length) {
+    } else if (event.submitter.id == 'all-grades-greater-than') {
+        let FilterValueNaN = isFilterValueNaN(filterValue);
+        if (FilterValueNaN) {
+            alert('Numbers Only');
+        } else {
+            for (i = 0; i < usersArray.length; i++) {
+                if ((Math.min(...usersArray[i].grades)) < filterValue) {
                     document.getElementById(usersArray[i].name).style.display = "none";
-
                 }
             }
         }
     }
-}
-)
-var form = document.getElementById('array-filter-and-manipulation');
-form.addEventListener('click', function (event) {
-    event.preventDefault();
-    filterValue = getFilterValue();
-    let FilterValueNaN = isFilterValueNaN(filterValue);
-
-    if (FilterValueNaN) {
-        alert('Numbers Only');
-    } else {
-
-        for (let i = 0; i < usersArray.length; i++) {
-            let avgGraed = averageGrades(usersArray[i].grades)
-            if (avgGraed > filterValue) {
-                document.getElementById(usersArray[i].name).style.display = "none";
-            } else if (filterValue > usersArray[i].address.houseNumber) {
-                document.getElementById(usersArray[i].name).style.display = "none";
-            } else {
-                let age = document.getElementById(usersArray[i].name).getElementsByClassName("age")[0];
-                filterValue = parseInt(filterValue);
-                usersArray[i].age = parseInt(usersArray[i].age);
-                age.textContent = usersArray[i].age + filterValue;
-                
+    else if (event.submitter.id == 'some-grades-greater-than') {
+        let FilterValueNaN = isFilterValueNaN(filterValue);
+        if (FilterValueNaN) {
+            alert('Numbers Only');
+        } else {
+            for (i = 0; i < usersArray.length; i++) {
+                for (j = 0; j < usersArray[i].grades[j]; j++) {
+                    if (usersArray[i].grades[j] > filterValue) {
+                        break;
+                    } else if (j + 1 == usersArray[i].grades.length) {
+                        document.getElementById(usersArray[i].name).style.display = "none";
+                    }
+                }
+            }
+        }
+    }
+    else if (event.submitter.id == 'array-filter-and-manipulation') {
+        let FilterValueNaN = isFilterValueNaN(filterValue);
+        if (FilterValueNaN) {
+            alert('Numbers Only');
+        } else {
+            for (i = 0; i < usersArray.length; i++) {
+                let avgGraed = averageGrades(usersArray[i].grades)
+                if (avgGraed > filterValue) {
+                    document.getElementById(usersArray[i].name).style.display = "none";
+                } else if (filterValue > usersArray[i].address.houseNumber) {
+                    document.getElementById(usersArray[i].name).style.display = "none";
+                } else {
+                    let age = document.getElementById(usersArray[i].name).getElementsByClassName("age")[0];
+                    filterValue = parseInt(filterValue);
+                    usersArray[i].age = parseInt(usersArray[i].age);
+                    age.textContent = usersArray[i].age + filterValue;
+                }
             }
         }
     }
