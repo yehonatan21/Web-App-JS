08219dc18a57d8e1588a7252c00fc00f18aefd96
Yehonatan <yehonatan349@gmail.com>
2022-07-10 14:03:13 +0300

    Implement Recursion

---
+++ recursion/recursion.js
@@ -0,0 +1,26 @@
+let form = document.getElementById('fib');
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    let n = document.getElementById('number-input').value;
+    n = fib(n)
+    addToDoc(n)
+})
+
+
+function fib(n) {
+    if (n == 1 || n == 0) {
+        return n;
+    }
+    else {
+        return fib(n - 1) + fib(n - 2)
+    }
+}
+
+function addToDoc(content) {
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add('card');
+    cardDiv.setAttribute("id", content);
+    cardDiv.innerHTML = content;
+    cardContainer.appendChild(cardDiv);
+}
+
---
+++ recursion/sierpinski-triangle.js
@@ -0,0 +1,45 @@
+const c = document.getElementById("canvas");
+const ctx = c.getContext("2d");
+
+const createTriangle = (pos, sidelen) => {
+    ctx.beginPath();
+    ctx.moveTo(...pos);
+
+    ctx.lineTo(pos[0] + sidelen / 2, pos[1] - sidelen * Math.sin(Math.PI / 3));
+    ctx.lineTo(pos[0] + sidelen, pos[1]);
+    ctx.lineTo(...pos);
+    ctx.closePath();
+    ctx.fill();
+};
+
+const createSierpinskiTriangle = (pos, sidelen, depth) => {
+    const innerTriangleSidelen = sidelen / 2;
+    const innerTrianglesPositions = [pos, [pos[0] + innerTriangleSidelen, pos[1]], [pos[0] + innerTriangleSidelen / 2, pos[1] - Math.sin(Math.PI / 3) * innerTriangleSidelen]]; // these positions are the same as what was used in the createTriangle function
+    if (depth == 0) {
+        innerTrianglesPositions.forEach((trianglePosition) => {
+            createTriangle(trianglePosition, innerTriangleSidelen);
+        });
+    } else {
+        innerTrianglesPositions.forEach((trianglePosition) => {
+            createSierpinskiTriangle(trianglePosition, innerTriangleSidelen, depth - 1);
+        });
+    }
+}
+
+let tr = document.getElementById('sierpinski-triangle');
+tr.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    let size = document.getElementById('size').value;
+    let amonut = document.getElementById('amount').value;
+    canvas.height = size;
+    canvas.width = size;
+
+    createSierpinskiTriangle([0, size], size, amonut); // pos(canvas size) - size in the canvas - amonut
+})
+
+
